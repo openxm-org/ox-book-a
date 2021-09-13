abd107636c556ea2d8e3ad1873423365139daa10
Katsuyoshi Ohara <ohara@se.kanazawa-u.ac.jp>
2021-09-13 16:23:42 +0900

    mpz_ptrの使用例

--- chap3/gmp/gmptest.c
+++ chap3/gmp/gmptest.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <gmp.h>
 
 int main()
 {
 	int m=31,n=7,exp=100;
-	mpz_t a,b,c;
+	mpz_t a,b;
+	mpz_ptr c;
 	mpz_init(a);      
 	mpz_init_set_ui(b,m);  /* b:=31 */
+	c = (mpz_ptr)malloc(sizeof(__mpz_struct));
 	mpz_init_set_ui(c,n);  /* c:=7 */
-
 	mpz_pow_ui(a,b,exp);   /* a:= b^100 */
 	gmp_fprintf(stdout,"%d^%d == %Zd\n",m,exp,a);
 
